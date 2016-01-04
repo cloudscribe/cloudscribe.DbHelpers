fa097c034f3a72bfe3e22bfe213bde382a2c0ff1
Joe Audette <joeaudette@users.noreply.github.com>
2016-01-04 15:13:32 -0500

    change default db path

--- src/cloudscribe.DbHelpers.SQLite/SqliteConnectionOptions.cs
+++ src/cloudscribe.DbHelpers.SQLite/SqliteConnectionOptions.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Author:					Joe Audette
 // Created:					2015-08-07
-// Last Modified:			2015-08-07
+// Last Modified:			2016-01-04
 // 
 
 
@@ -15,7 +15,7 @@ namespace cloudscribe.DbHelpers.SQLite
 
         public string ConnectionString { get; set; } = string.Empty;
 
-        public string PathSegment { get; set; } = "/config/sqlitedb/";
+        public string PathSegment { get; set; } = "/cloudscribe_config/sqlitedb/";
 
     }
 }
